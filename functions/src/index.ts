283469f841d14ab6be95db98603216575edf1c24
= <=>
2019-02-27 17:49:21 +0530

    Add Functions For Timer Notifications

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions';
+import * as fetch from 'node-fetch';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,36 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const app = admin.initializeApp();
+
+export const handleTimer = functions.https.onRequest((req, res) => {
+    fetch('http://localhost:3000/timer', {
+        method: 'post',
+        body:    JSON.stringify(req.body),
+        headers: { 'Content-Type': 'application/json' },
+    })
+        .then(resp => resp.text())
+        .then(text => {
+            res.sendStatus(200);
+        })
+        .catch(e => res.sendStatus(500))
+});
+
+export const handleFirstNotification = functions.https.onRequest((req, res) => {
+    const data = req.body;
+    const regId = data.registration_id;
+
+    const payload = {
+        'notification': {
+            'body': data.body,
+            'title': data.title
+        }
+    };
+
+    app.messaging().sendToDevice(regId, payload)
+        .then(resp => {
+            res.sendStatus(200);
+        })
+        .catch(err => res.sendStatus(500));
+});
